b567f2e5ac206c9b0cda7767f700cf33ca1ac266
Andrea Soldà <a.solda@studenti.unisa.it>
2016-05-05 09:24:01 +0200

    New test for best_response bot

--- testGSP.py
+++ testGSP.py
@@ -1,18 +1,57 @@
 #!/usr/bin/python
 
-from GSP import gsp
+from GSP import gsp, best_response
 
 #Slots' clickthrough rates
 slot_ctrs=dict()
 slot_ctrs["id1"] = 10
 slot_ctrs["id2"] = 4
 
-#Advertisers' bids
+#Advertisers' values
+adv_values=dict()
+adv_values["x"] = 7
+adv_values["y"] = 6
+adv_values["z"] = 1
+
+#Advertisers' bots
+adv_bots=dict()
+adv_bots["x"] = best_response
+adv_bots["y"] = best_response
+adv_bots["z"] = best_response
+
+step=0
+history=[]
 adv_bids=dict()
-adv_bids["x"] = 7
-adv_bids["y"] = 6
-adv_bids["z"] = 1
 
-adv_slots, adv_pays = gsp(slot_ctrs, adv_bids)
+done=False
+max_step=100
 
-print(adv_slots, adv_pays)
+#We repeat the auctions as long as an equilibrium has not been reached.
+#(This mean that advertisers submit the same bids in any successive repetition.)
+#If an equilibrium is not reached in short time, then we stop after max_step steps
+while not done and step < max_step:
+        
+        done = True
+        for i in adv_values.keys():
+            #Invoke the bots for computing the bids for each advertiser
+            adv_bids[i] = adv_bots[i](i,adv_values[i],slot_ctrs,history)
+            #If it is the first step or there is at least one advertiser whose bid is different from the bid submitted in the previous step,
+            #then we need another iteration, otherwise we can stop
+            if step == 0 or adv_bids[i] != history[step-1]["adv_bids"][i]:
+                done=False
+                
+        if done:
+            break
+        
+        #Execute the GSP auction with the bids computed above
+        adv_slots, adv_pays = gsp(slot_ctrs,adv_bids)
+        
+        #Update the history
+        history.append(dict())
+        history[step]["adv_bids"]=dict(adv_bids)
+        history[step]["adv_slots"]=dict(adv_slots)
+        history[step]["adv_pays"]=dict(adv_pays)
+        
+        print(step, history[step])
+        
+        step += 1
